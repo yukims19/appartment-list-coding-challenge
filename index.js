04749ee666e1579f23a5aa38f493b54d0927cae4
yukims19 <yukims19@gmail.com>
2019-03-25 17:09:59 -0700

    Revert "Alternative and refactory"
    This reverts commit 2821f556eccbe50d992d681c333d0b23527ee4a3.

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ let familyGroup = array => {
   while (newArray.length > 0) {
     if (group.length < 3) {
       let randomIdx = Math.floor(Math.random() * Math.floor(newArray.length));
+      console.log("randomIdx:", randomIdx);
       group.push(newArray[randomIdx]);
-      /*newArray[randomIdx] = null;
-      newArray = newArray.filter(ele => ele !== null);*/
-      newArray.splice(randomIdx, 1);
+      newArray[randomIdx] = null;
+      newArray = newArray.filter(ele => ele !== null);
     } else {
       groups.push(group);
       group = [];
@@ -25,73 +25,9 @@ let familyGroup = array => {
   return groups;
 };
 
-/*Here is a refactory for the familyGroup*/
-let takeRandom = array => {
-  let randomIdx = Math.floor(Math.random() * Math.floor(array.length));
-  return array.splice(randomIdx, 1);
-};
-
-let addToGroup = (group, person) => {
-  return group.concat(person);
-};
-
-let groupConcatWithLastGroup = (groupsArray, group) => {
-  groupsArray[groupsArray.length - 1] = groupsArray[
-    groupsArray.length - 1
-  ].concat(group);
-  return groupsArray;
-};
-
-let familyGroupRefactor = people => {
-  let newPeople = people.slice();
-  let groups = [];
-  let group = [];
-  let defaultGroupSize = 3;
-  let groupSizeLowerBound = 3;
-  let groupSizeUpperBound = 5;
-
-  while (newPeople.length > 0) {
-    if (group.length < defaultGroupSize) {
-      group = addToGroup(group, takeRandom(newPeople));
-    } else {
-      groups.push(group);
-      group = [];
-    }
-  }
-
-  //Handle last generated group
-  let isGroupTooSmall = group.length > 0 && group.length < groupSizeLowerBound;
-  if (isGroupTooSmall) {
-    groups = groupConcatWithLastGroup(groups, group);
-  } else if (group.length > 0) {
-    groups.push(group);
-  }
-  return groups;
-};
-
-/*Alternative solution of shuffling the array first*/
-let shuffleArray = array => {
-  return array.sort(() => Math.random() - 0.5);
-};
-let familyGroup2 = people => {
-  let shuffledPeople = shuffleArray(people.slice());
-  let groups = [];
-  let defaultGroupSize = 3;
-  let groupSizeLowerBound = 3;
-  let groupSizeUpperBound = 5;
-
-  while (shuffledPeople.length > 0) {
-    if (shuffledPeople.length < groupSizeLowerBound) {
-      groupConcatWithLastGroup(
-        groups,
-        shuffledPeople.splice(0, defaultGroupSize)
-      );
-    } else {
-      groups.push(shuffledPeople.splice(0, defaultGroupSize));
-    }
-  }
-  return groups;
-};
+//input -> ["",]
+//outout =>[["","",""]]
+//default size => 3
 
 let people = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(familyGroup2(people));
+console.log(familyGroup(people));
